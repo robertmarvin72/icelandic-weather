587ada5fbc4eed091a794316387499836eb7e08b
Robert <robbinn@gmail.com>
2026-01-07 17:36:29 +0000

    fixes #59 CI tests and coverage

--- vitest.config.js
+++ vitest.config.js
@@ -5,5 +5,19 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/test/setupTests.js"],
+
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+
+      exclude: [
+        "node_modules/",
+        "src/test/",
+        "**/*.config.*",
+        "**/*.d.ts",
+        "src/main.jsx",
+      ],
+    },
   },
 });
